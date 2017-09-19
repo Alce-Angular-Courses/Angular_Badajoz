a367491bed532b3faf64e336e5c13be07d4c4c2d
alce65 <alce65@hotmail.es>
2017-09-19 15:01:40 +0200

    Vistas. Correcciones

--- 05a_Muestrario/js/appModule.js
+++ 05a_Muestrario/js/appModule.js
@@ -1,10 +1,2 @@
-angular.module("appModule", [])
-.controller('AppController', ['$scope', function($scope){
 
-	$scope.titulo = "Muestrario";
-	$scope.isHidden = true;
-	$scope.btnColores = function() {
-		$scope.isHidden = !$scope.isHidden;
-	};	
-	
-}])
+angular.module("appModule", [])
--- 05b_Muestrario_2/js/appModule.js
+++ 05b_Muestrario_2/js/appModule.js
@@ -1,9 +1,1 @@
-angular.module("appModule", [])
-.controller('AppController', ['$scope', function($scope){
-
-	$scope.isHidden = true;
-	$scope.verColores = function(verOK) {
-		$scope.isHidden = verOK;
-	};	
-	
-}])
+angular.module("appModule", []);
